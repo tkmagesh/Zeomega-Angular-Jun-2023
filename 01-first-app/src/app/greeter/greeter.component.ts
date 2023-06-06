d1bcf5f3c51b0e742b96282baf8463df2e9c4b9b
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-06 17:27:07 +0530

    greeter - v2

--- 01-first-app/src/app/greeter/greeter.component.ts
+++ 01-first-app/src/app/greeter/greeter.component.ts
@@ -5,10 +5,29 @@ import { Component } from "@angular/core";
     templateUrl : 'greeter.component.html'
 })
 export class GreeterComponent{
+    message: string = '';
 
-    message : string = '';
-
+    // version - 1
+    /* 
     onBtnGreetClick(userName : string) {
-        this.message = `Hi ${userName}, Have a nice day!`
+        this.message = `Hi ${userName}, Have a nice day! [v1]`
+    } 
+    */
+
+
+    /* version-2 */
+    private _userName : string = ''
+
+    get userName() : string {
+        return this._userName
     }
+
+    set userName(val : string){
+        this._userName = val;
+    }
+    
+    onBtnGreetClick() {
+        this.message = `Hi ${this.userName}, Have a nice day! [v2]`
+    }
+
 }

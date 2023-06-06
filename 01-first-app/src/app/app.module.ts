c65da7e29a77325889dbc8829154bdfb0821a730
Magesh Kuppan <tkmagesh77@gmail.com>
2023-06-06 16:53:51 +0530

    greeter setup

--- 01-first-app/src/app/app.module.ts
+++ 01-first-app/src/app/app.module.ts
@@ -2,17 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GreeterComponent } from './greeter/greeter.component';
 
 @NgModule({
   /* Register the UI entities (component, pipe & directive) of the module */
   declarations: [
-    AppComponent
+    AppComponent,
+    GreeterComponent
   ],
   imports: [
     BrowserModule
   ],
   /* Register the "services" */
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [
+    AppComponent,
+    GreeterComponent
+  ]
 })
 export class AppModule { }
